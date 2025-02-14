6828a31276f094600316f3c4f4ff66f7e5797ab4
Yared Yemane <yaredyemane1@gmail.com>
2025-02-14 10:48:10 +0300

    copyright

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -4,12 +4,17 @@ import LeftFooterSection from "./LeftFooterSection";
 
 const Footer = () => {
   return (
-    <div className=" flex justify-between gap-5 h-auto bg-blue-950 py-5">
-      <div className=" w-full">
-        <LeftFooterSection />
+    <div className="flex flex-col bg-blue-950 py-5">
+      <div className="flex justify-between gap-5 px-10">
+        <div className="w-full">
+          <LeftFooterSection />
+        </div>
+        <div className="w-full">
+          <FeedbackForm />
+        </div>
       </div>
-      <div className=" w-full">
-        <FeedbackForm />
+      <div className="border-t border-gray-700 mt-5 pt-4 text-center text-gray-300 text-sm">
+        © {new Date().getFullYear()} AfroTrust. All rights reserved.
       </div>
     </div>
   );
